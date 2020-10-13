e19d52dc9b4702e05567333318db7335c5cdc624
donad <dona.daoud14@gmail.com>
2020-10-13 20:46:21 +0300

    ..

--- app/src/main/java/daoud/dona/taskydona/MainActivity.java
+++ app/src/main/java/daoud/dona/taskydona/MainActivity.java
@@ -3,12 +3,14 @@ package daoud.dona.taskydona;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
--- app/src/main/java/daoud/dona/taskydona/SignUpActvity.java
+++
@@ -1,4 +0,0 @@
-package daoud.dona.taskydona;
-
-public class SignUpActvity {
-}
